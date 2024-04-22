0f0fe54588bfd2c48fbe6995b490cd6a500db5a1
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2024-04-22 15:19:25 -0400

    data

--- visualization.py
+++ visualization.py
@@ -22,63 +22,34 @@ rewards = {
 }
 
 
-def read_and_plot_csv(file_path):
-    df = pd.read_csv(file_path)
-
-    plt.figure()
-    plt.plot(df["episode"], df["reward"])
-    plt.title("Reward per Episode")
-    plt.xlabel("Episode")
-    plt.ylabel("Reward")
-    plt.xlim(left=0)
-    plt.grid(True)
-    plt.show()
-
-
-def group_and_plot_epochs(file_path, n):
-    file_name = os.path.basename(file_path)
-    parts = file_name.replace(".csv", "").split("_")
-    game = full_name_dict.get(parts[0].lower(), parts[0])
-    model = full_name_dict.get(parts[1].lower(), parts[1])
-    variation = full_name_dict.get(parts[2].lower(), parts[2])
-    title = f"{game} - {model} - {variation} - Average Reward per Epoch ({n} episodes per epoch)"
-
-    df = pd.read_csv(file_path)
-
-    df["epoch"] = (df["episode"] - 1) // n
-    epoch_data = df.groupby("epoch")["reward"].mean().reset_index()
-
-    epoch_data["epoch"] += 1
-    epoch_data["reward"] = epoch_data["reward"] + abs(rewards[game][0])
-
-    start_point = pd.DataFrame({"epoch": [0], "reward": [0]})
-    epoch_data = pd.concat([start_point, epoch_data], ignore_index=True)
-
-    plt.figure(figsize=(10, 5))
-    plt.plot(
-        epoch_data["epoch"],
-        epoch_data["reward"],
-        linestyle="-",
-        color="blue",
-    )
-    plt.title(title)
-    plt.xlabel("Epoch")
-    plt.ylabel("Average Reward")
-    plt.xlim(left=0, right=epoch_data["epoch"].max())
-    plt.ylim(bottom=0, top=rewards[game][1] - rewards[game][0])
-    plt.grid(True)
-    plt.savefig(f"./out/{game}_{model}_{variation}.png")
-
-    performance_analysis(epoch_data, game, title)
-    save_epoch_data_to_csv(epoch_data, game, model, variation)
-
-
 def save_epoch_data_to_csv(epoch_data, game, model, variation):
     csv_filename = f"./out/data/{game}_{model}_{variation}_epoch_data.csv".replace(
         " ", "_"
     )
     epoch_data.to_csv(csv_filename, index=False)
     print(f"Saved epoch data to {csv_filename}")
+
+
+def process_file(file_path, episode_per_epoch):
+    file_name = os.path.basename(file_path)
+    parts = file_name.replace(".csv", "").split("_")
+    game = full_name_dict.get(parts[0].lower(), parts[0])
+    model = full_name_dict.get(parts[1].lower(), parts[1])
+    variation = full_name_dict.get(parts[2].lower(), parts[2])
+    title = f"{game} - {model} - {variation} - Average Reward per Epoch ({episode_per_epoch} episodes per epoch)"
+
+    df = pd.read_csv(file_path)
+    df["epoch"] = (df["episode"] - 1) // episode_per_epoch
+    epoch_data = df.groupby("epoch")["reward"].mean().reset_index()
+    epoch_data["epoch"] += 1
+    epoch_data["reward"] = epoch_data["reward"] + abs(rewards[game][0])
+
+    start_point = pd.DataFrame({"epoch": [0], "reward": [0]})
+    epoch_data = pd.concat([start_point, epoch_data], ignore_index=True)
+
+    # save_epoch_data_to_csv(epoch_data, game, model, variation)
+
+    return epoch_data, game, model, variation, title
 
 
 def performance_analysis(epoch_data, game, title):
@@ -102,29 +73,45 @@ def performance_analysis(epoch_data, game, title):
     print(f"Max score: {max_score} at epoch {max_score_epoch}")
 
 
-def process_file(file_path, n):
-    file_name = os.path.basename(file_path)
-    parts = file_name.replace(".csv", "").split("_")
-    game = full_name_dict.get(parts[0].lower(), parts[0])
-    model = full_name_dict.get(parts[1].lower(), parts[1])
-    variation = full_name_dict.get(parts[2].lower(), parts[2])
-    title = f"{game} - {model} - {variation} - Average Reward per Epoch ({n} episodes per epoch)"
-
+def plot_raw_episodes(file_path):
     df = pd.read_csv(file_path)
-    df["epoch"] = (df["episode"] - 1) // n
-    epoch_data = df.groupby("epoch")["reward"].mean().reset_index()
-    epoch_data["epoch"] += 1
-    epoch_data["reward"] = epoch_data["reward"] + abs(rewards[game][0])
 
-    save_epoch_data_to_csv(epoch_data, game, model, variation)
+    plt.figure()
+    plt.plot(df["episode"], df["reward"])
+    plt.title("Reward per Episode")
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.xlim(left=0)
+    plt.grid(True)
+    plt.show()
 
-    return epoch_data, game, model, variation, title
+
+def plot_epochs(file_path, episode_per_epoch):
+    epoch_data, game, model, variation, title = process_file(
+        file_path, episode_per_epoch
+    )
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(
+        epoch_data["epoch"],
+        epoch_data["reward"],
+        linestyle="-",
+        color="blue",
+    )
+    plt.title(title)
+    plt.xlabel("Epoch")
+    plt.ylabel("Average Reward")
+    plt.xlim(left=0, right=epoch_data["epoch"].max())
+    plt.ylim(bottom=0, top=rewards[game][1] - rewards[game][0])
+    plt.grid(True)
+    plt.savefig(f"./out/{game}_{model}_{variation}.png")
+
+    performance_analysis(epoch_data, game, title)
 
 
 def plot_all_experiments(files, n):
     game_data = {}
 
-    # Process each file and organize data by game
     for file in files:
         epoch_data, game, model, variation, title = process_file(
             os.path.join(result_folder, file), n
@@ -133,7 +120,6 @@ def plot_all_experiments(files, n):
             game_data[game] = []
         game_data[game].append((epoch_data, model, variation, title))
 
-    # Plot data for each game
     for game, data_list in game_data.items():
         plt.figure(figsize=(10, 5))
         for epoch_data, model, variation, title in data_list:
@@ -154,10 +140,10 @@ def plot_all_experiments(files, n):
 
 
 result_folder = "./results"
-files = [
-    f
-    for f in os.listdir(result_folder)
-    if os.path.isfile(os.path.join(result_folder, f))
-]
+
+files = []
+for f in os.listdir(result_folder):
+    if os.path.isfile(os.path.join(result_folder, f)):
+        files.append(f)
 
 plot_all_experiments(files, 10)
